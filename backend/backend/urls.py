ce526354c16df557471b9ca8327e7995efe2f9ee
rares1215 <raresgiurgescu@gmail.com>
2025-12-02 20:29:05 +0200

    configed the DRF app, added auth logic with JWT tokens, created CustomUser model in api.models, connected our psql database to our project

---
+++ backend/backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='access_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+]
