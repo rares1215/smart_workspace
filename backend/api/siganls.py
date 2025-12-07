86cd296e18c79ace07b63e88f127392867702dbb
rares1215 <raresgiurgescu@gmail.com>
2025-12-07 18:55:10 +0200

    Added siganls.py and created 2 functions, one to get the text on creation and the other to delete document from media when we delete the instance corespondig to it

--- backend/api/siganls.py
+++ backend/api/siganls.py
@@ -3,3 +3,27 @@ from django.db.models.signals import post_save,post_delete
 from .models import DocumentUpload
 from .utils.analyze_text_with_llm import analyze_text
 from .utils.extract_text_from_pdf import extract_text
+import os
+
+
+@receiver(post_save,sender=DocumentUpload)
+def save_text_on_upload(sender,instance,created, **kwargs):
+    if not created:
+        return
+    
+    text = extract_text(instance.doc_file.path)
+    instance.text = text
+    DocumentUpload.objects.filter(id=instance.id).update(text=text)
+
+
+
+@receiver(post_delete, sender=DocumentUpload)
+def delete_file_after_deleting_model(sender,instance,**kwargs):
+    if instance.doc_file and hasattr(instance.doc_file,'path'):
+        try:
+            file_path = instance.doc_file.path
+            if os.path.isfile(file_path):
+                print("Removed document from media!")
+                os.remove(file_path)
+        except:
+            print(f"Could not delete document with path:{file_path}")
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterFormViewSet
+from .views import RegisterFormViewSet,FileUploadView
 
 urlpatterns = [
-    path('register/', RegisterFormViewSet.as_view(), name='register_user')
+    path('register/', RegisterFormViewSet.as_view(), name='register_user'),
+    path('upload/', FileUploadView.as_view(), name='upload_file'),
 ]
